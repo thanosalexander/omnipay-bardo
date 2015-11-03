9e81f1ac4e22c4db79718ebf527de26439786093
Ivan Kipyegon <ivan@bardo.com>
2015-11-03 12:38:20 +0300

    added claback url function calling

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -6,25 +6,35 @@ use Omnipay\Common\Message\AbstractRequest;
 use Omnipay\Common\Exception\InvalidRequestException;
 
 /**
- * Migs Complete Purchase Request
+ * Bardo Complete Purchase Request
  */
 class CompletePurchaseRequest extends AbstractRequest
 {
-	public function getData()
-	{
+    public function getData()
+    {
 		
-		$data = $this->httpRequest->query->all();
-
+        
+		$shop = $this->httpRequest->query->all();
+		$shopnumber = $shop['SHOP_NUMBER'];
+        
+		
+		$data = $this->httpClient->post('https://pay.bardo-gateway.com/trm/TransactionHandler.ashx')
+		->setPostField('UserName', 'your_given_username')
+		->setPostField('Password', 'your_given_password')
+		->setPostField('SHOP_NUMBER', $shopnumber)
+		->send();
+		
 		return $data;
-	}
+		
+    }
 
-	public function sendData($data)
-	{
-		return $this->response = new CompletePurchaseResponse($this, $data);
-	}
+    public function sendData($data)
+    {
+        return $this->response = new CompletePurchaseResponse($this, $data->json());
+    }
 
-	public function getEndpoint()
-	{
-		return $this->endpoint;
-	}
+    public function getEndpoint()
+    {
+        return $this->endpoint;
+    }
 }
